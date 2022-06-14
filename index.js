d963a18ac8b672981962f05efe2b1a122d3657ca
DevopsAdmin786 <106879991+DevopsAdmin786@users.noreply.github.com>
2022-06-14 11:44:02 +0530

    Update index.js
    New code

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Testing DevOps Node! JS Project Hello How are you ? welcome to devOps Project. I am farman ansari duing the testings')
+  response.send('Hello Testing DevOps Node! JS Project Hello How are you ? welcome to devOps Project. I am Arun Sir duing the testings')
 }) 
 
 app.listen(app.get('port'), function() {
